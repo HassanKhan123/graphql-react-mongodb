7b5a0cf2257c7d09d3435c615d22e36b3fd7ce21
HassanKhan123 <hk2602809@gmail.com>
2022-06-11 11:59:12 +0500

    added: mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const colors = require("colors");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 
 const schema = require("./schema/schema");
+const connectDB = require("./config/db");
 
 const app = express();
+
+// connect to database
+connectDB();
+
 const port = process.env.PORT || 5000;
 
 app.use(
